9a0f12ea0472230977521d40c7ef57a948d28cf0
Doflatango <hst0012010@gmail.com>
2024-06-03 20:15:32 +0800

    [tencent_cos_plus] feat: 支持使用临时安全凭证

--- packages/tencent_cos_plus/lib/src/model/common/cos_config.dart
+++ packages/tencent_cos_plus/lib/src/model/common/cos_config.dart
@@ -4,6 +4,7 @@ class COSConfig {
     required this.appId,
     required this.secretId,
     required this.secretKey,
+    this.sessionToken,
   });
 
   /// 开发者访问 COS 服务时拥有的用户维度唯一资源标识
@@ -14,4 +15,7 @@ class COSConfig {
 
   /// 开发者拥有的项目身份密钥
   String secretKey;
+
+  /// 如果使用临时安全凭证必须传入该参数
+  String? sessionToken;
 }
